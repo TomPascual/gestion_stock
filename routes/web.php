4ca0bf6e8db25a7c99afd0164e437c0d02445898
Tom <tom.pascual@aftec.org>
2025-02-22 16:11:41 +0100

    Ajout de la redirection de / vers /produits

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,10 @@
 use App\Http\Controllers\ProduitController;
 use Illuminate\Support\Facades\Route;
 
+// Redirection de la page d'accueil vers la liste des produits
+Route::get('/', function () {
+    return redirect()->route('produits.index');
+});
+
+// Ressources pour les produits
 Route::resource('produits', ProduitController::class);

665b599b9d5655def958ecf0871c01fc2b66979a
Tom <tom.pascual@aftec.org>
2025-03-09 17:02:13 +0100

    Ajout des routes pour la gestion des fournisseurs

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProduitController;
 use App\Http\Controllers\MouvementStockController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FournisseurController;
 
 // Page d'accueil qui redirige vers Home
 Route::get('/', function () {
@@ -14,7 +15,8 @@ Route::resource('produits', ProduitController::class);
 Route::get('produits/{produit}/mouvements', [ProduitController::class, 'mouvements'])->name('produits.mouvements');
 Route::post('produits/{produit}/ajouter-mouvement', [ProduitController::class, 'ajouterMouvement'])->name('produits.ajouterMouvement');
 
-
+// Routes pour les fournisseurs
+Route::resource('fournisseurs', FournisseurController::class);
 
 // Historique des mouvements
 Route::get('/mouvements', [MouvementStockController::class, 'index'])->name('mouvements.index');
